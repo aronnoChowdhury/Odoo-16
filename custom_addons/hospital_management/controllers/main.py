daa5f712a96f4555887ee7a860ee03e39e3e6a4a
Aronno Chowdhury <aronnochowdhury220@gmail.com>
2026-09-07 00:29:26 +0600

    Frontend Worked

--- custom_addons/hospital_management/controllers/main.py
+++ custom_addons/hospital_management/controllers/main.py
@@ -1,21 +1,40 @@
-from odoo import http
+from odoo import http 
 from odoo.http import request
 
 class HospitalWebsite(http.Controller):
 
-    @http.route('/hospital/appointments', type="http", auth="public", website=True)
+    @http.route('/hospital/appointments', type='http', auth='public', website=True)
     def hospital_appointments(self, *kw):
         doctors = request.env['hospital.doctor'].sudo().search([])
-        return request.render('hospital_management.hospital_appointments', {'doctors': doctors})
+
+        return request.render('hospital_management.hospital_appointment_web_form', {'doctors': doctors})
+
+    @http.route('/hospital/appointment/submit', type='http', auth='public', methods=['POST'], website=True, csrf=False)
+    def hospital_appointment_submit(self, **post):
+        patient_name = post.get('patient_name')
+        doctor_id = post.get('doctor_id')
+        appointment_date = post.get('appointment_date')
+
+        patient = request.env['hospital.patient'].sudo().search([
+            ('full_name', '=', patient_name),
+        ],limit=1)
 
 
-    @http.route('/hospital/appointment/submit', type="http", auth="public", methods=['POST'], website=True, csrf=False)
-    def hospital_appointment_submit(self,**post):
+        if patient: 
+            if appointment_date:
+                appointment_date = appointment_date.replace('T', ' ')
 
-        request.env['hospital.appointment'].sudo().create({
-            'patient_id': post.get('patient_name'),
-            'doctor_id': int(post.get('doctor_id')),
-            'appointment_date': post.get('appointment_date'),
+            request.env['hospital.appointment'].sudo().create({
+                'patient_id': patient.id, 
+                'doctor_id': int(doctor_id), 
+                'appointment_date': appointment_date,
+            })
+
+            return request.render('hospital_management.hospital_appointment_thankyou', {})
+
+        doctors = request.env['hospital.doctor'].sudo().search([])
+
+        return request.render('hospital_management.hospital_appointment_web_form', {
+            'doctors': doctors,
+            'error': 'Patient not found. Please register as a patient first.'
         })
-
-        return request.render('hospital_management.hospital_appointment_thankyou', {})
